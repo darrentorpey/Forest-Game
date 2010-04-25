16257594750147c4fc21a393c6914b54a24429a2
Darren Torpey <dct@punchbowlsoftware.com>
2010-04-25 14:48:30 -0400

    Small tweaks to map and water-flowing bug fix

--- first_game/blue.js
+++ first_game/blue.js
@@ -21,13 +21,13 @@ BLUE_MANAGER = {
       var next_left  = maze.map[blue_tile.y][blue_tile.x - 1];
       var far_left   = maze.map[blue_tile.y][blue_tile.x - 2];
 
-      if (next_up == BLUE_PATH && (far_up == null || far_up == BLUE_PATH)) {
+      if (next_up == BLUE_PATH && !is_wall(far_up)) {
         this.grow_into({ x: blue_tile.x, y: blue_tile.y - 1 });
-      } else if (next_left == BLUE_PATH && (far_left == null || far_left == BLUE_PATH)) {
+      } else if (next_left == BLUE_PATH && !is_wall(far_left)) {
         this.grow_into({ x: blue_tile.x - 1, y: blue_tile.y });
-      } else if (next_right == BLUE_PATH && (far_right == null || far_right == BLUE_PATH)) {
+      } else if (next_right == BLUE_PATH && !is_wall(far_right)) {
         this.grow_into({ x: blue_tile.x + 1, y: blue_tile.y });
-      } else if (next_down == BLUE_PATH && (far_down == null || far_down == BLUE_PATH)) {
+      } else if (next_down == BLUE_PATH && !is_wall(far_down)) {
         this.grow_into({ x: blue_tile.x, y: blue_tile.y + 1 });
       }
     }
@@ -37,4 +37,8 @@ BLUE_MANAGER = {
     help.setTileInMap(gbox.getCanvasContext('mazecanvas'), maze, coords.x, coords.y, BLUE_TILE);
     this.blue_tiles.push({ x: coords.x, y: coords.y });
   }
+}
+
+function is_wall(tile) {
+  return any_match([0, 1, 2, 3, 4, 5, 6, 7, 8], tile);
 }
